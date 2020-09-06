fc63cefc4130d75a02438c59ab07ad7cd23959ea
Washi <washi1337@protonmail.com>
2020-09-06 13:21:07 +0200

    Add tests folder.

---
+++ test/BfHs/TapeTest.hs
@@ -0,0 +1,7 @@
+module BfHs.TapeTest where
+
+import Test.HUnit
+import BfHs.Tape
+
+
+test1 = TestCase $ assertEqual "my message" 1 2
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,7 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import BfHs.TapeTest
+
+tests = TestList [test1]
+
+main :: IO Counts
+main = runTestTT tests
