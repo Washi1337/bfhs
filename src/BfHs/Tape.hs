eebd664576690ecb07689dca1354ac4f9b2a21cc
Washi <washi1337@protonmail.com>
2020-09-06 13:49:01 +0200

    Add read/write/move right.

--- src/BfHs/Tape.hs
+++ src/BfHs/Tape.hs
@@ -2,6 +2,11 @@ module BfHs.Tape where
 
 {-|
     The main tape data constructor.
+
+    The constructor consists of three parts:
+    - The left hand side of the data pointer (reversed)
+    - The value at the current data pointer
+    - THe right hand side of the data pointer.
 -}
 data Tape a = Tape [a] a [a]
     deriving (Show)
@@ -9,5 +14,26 @@ data Tape a = Tape [a] a [a]
 instance (Eq a) => Eq (Tape a) where
     (Tape l1 c1 r1) == (Tape l2 c2 r2) = (l1 == l2) && (c1 == c2) && (r1 == r2)
 
+{-|
+    Reads the cell from the tape at the current data pointer.
+-}
+readCell :: Tape a -> a
+readCell (Tape _ cell _) = cell
+
+{-|
+    Writes a value into the cell on the tape at the current data pointer.
+-}
+writeCell :: Tape a -> a -> Tape a
+writeCell (Tape left _ right) value = Tape left value right
+
+{-|
+    Moves the data pointer to the left.
+-}
 moveLeft :: Tape a -> Tape a
-moveLeft (Tape (l:ls) c rs) = Tape ls l (c:rs)
+moveLeft (Tape (l:ls) cell right) = Tape ls l (cell:right)
+
+{-|
+    Moves the data pointer to the right.
+-}
+moveRight :: Tape a -> Tape a
+moveRight (Tape left cell (r:rs)) = Tape (cell:left) r rs
--- test/BfHs/TapeTest.hs
+++ test/BfHs/TapeTest.hs
@@ -5,12 +5,37 @@ import BfHs.Tape
 
 tapeTests :: Test
 tapeTests = TestList [ 
-        moveLeftTest 
+        readCellTest,
+        writeCellTest,
+        moveLeftTest,
+        moveRightTest
     ]
 
-moveLeftTest :: Test
-moveLeftTest = TestCase (assertEqual "Initial test" test expected)
+readCellTest :: Test
+readCellTest = TestCase (assertEqual "Should read the current value." test expected)
     where 
-        initial     = Tape [3,2,1] 4 [5,6,7] :: Tape Int
-        test        = moveLeft initial
-        expected    = Tape [2,1] 3 [4,5,6,7]
+        initialState = Tape [3,2,1] 4 [5,6,7]
+        test         = readCell initialState
+        expected     = 4
+
+writeCellTest :: Test
+writeCellTest = TestCase (assertEqual "Should replace the current value." test expected)
+    where 
+        initialState = Tape [3,2,1] 4 [5,6,7]
+        newValue     = 10
+        test         = writeCell initialState newValue
+        expected     = Tape [3,2,1] newValue [5,6,7]
+
+moveLeftTest :: Test
+moveLeftTest = TestCase (assertEqual "Should move to the left." test expected)
+    where 
+        initialState = Tape [3,2,1] 4 [5,6,7]
+        test         = moveLeft initialState
+        expected     = Tape [2,1] 3 [4,5,6,7]
+
+moveRightTest :: Test
+moveRightTest = TestCase (assertEqual "Should move to the right." test expected)
+    where 
+        initialState = Tape [3,2,1] 4 [5,6,7]
+        test         = moveRight initialState
+        expected     = Tape [4,3,2,1] 5 [6,7]
